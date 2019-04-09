ee13fb7a2fe672914d3aa232cc2a99d8e557a90e
atlansien <atlansien@yahoo.co.jp>
2019-04-10 00:09:44 +0900

    days.jsのインストール

--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,26 @@
-const Comments = [];
+const daysjs = require("dayjs");
 
-const nextId = 1;
+const comments = [];
+
+let nextId = 1;
 
 class Comment {
   constructor({ username, body }) {
     (this.id = nextId++),
       (this.username = username),
       (this.body = body),
-      (this.createdAt = new Date()),
-      (this.updatedAt = new Date());
+      (this.createdAt = daysjs().format("YYYY年MM月DD日 HH:mm:ss")),
+      (this.updatedAt = daysjs().format("YYYY年MM月DD日 HH:mm:ss"));
   }
 }
+
+// テスト、確認用に配列に挿入するDBの作成
+for (let i = 0; i < 3; i++) {
+  const comment = new Comment({
+    username: "username" + i,
+    body: "body" + i
+  });
+  comments.push(comment);
+}
+
+module.exports = {};

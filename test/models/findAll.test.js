367daf5168a2046c1c7f6a0399e0326c70e7e7d0
atlansien <atlansien@yahoo.co.jp>
2019-04-13 08:53:52 +0900

    commentsは配列である、適切にデータが格納されているテスト

--- test/models/findAll.test.js
+++ test/models/findAll.test.js
@@ -1,12 +1,23 @@
 const assert = require('power-assert');
 const comment = require('../../models/Comment');
 
-describe('findAllのテスト', () => {
-    const comments = comment.findAll();
+describe('comment.findAllのテスト', () => {
+  const comments = comment.findAll();
   it('findAllはメソッドである', () => {
     assert.equal(typeof comment.findAll, 'function');
   });
   it('commentsは配列である', () => {
-      assert.equal(Array.isArray(comments), true)
-  })
+    assert.equal(Array.isArray(comments), true);
+  });
+  it('commentsには適切にデータが格納されている', () => {
+    comments.forEach(comment => {
+      assert.deepEqual(comment, {
+        id: comment.id,
+        username: comment.username,
+        body: comment.body,
+        createdAt: comment.createdAt,
+        updatedAt: comment.updatedAt
+      });
+    });
+  });
 });

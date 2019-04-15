368947e5828463952e25ef5a46af68445b8c7bf6
atlansien <atlansien@yahoo.co.jp>
2019-04-15 18:33:55 +0900

    idの引数に適切でない数値が入っていた場合のテスト、現在RED

--- test/models/updateComment.test.js
+++ test/models/updateComment.test.js
@@ -5,4 +5,26 @@ describe('Comment.updateCommentのテスト', () => {
   it('Comment.updateCommentはメソッドである', () => {
     assert.equal(typeof Comment.updateComment, 'function');
   });
+  it('idの引数に不正な値が入っていた場合エラーが返される', () => {
+    const invalidIdList = [
+      { id: 0 },
+      { id: -1 },
+      { id: null },
+      { id: {} },
+      { id: [] },
+      { id: '1' },
+    ];
+
+    invalidIdList.forEach(id => {
+      try {
+        Comment.updateComment(id);
+        assert.fail();
+      } catch (error) {
+        assert.equal(
+          error.message,
+          'id値に適切でない値が入っています、1以上の数字を入れてください'
+        );
+      }
+    });
+  });
 });

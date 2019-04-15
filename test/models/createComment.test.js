7faa0b236a7c1766f52a7a20c24f0de6d6d2ac10
atlansien <atlansien@yahoo.co.jp>
2019-04-15 15:25:08 +0900

    小修正

--- test/models/createComment.test.js
+++ test/models/createComment.test.js
@@ -1,8 +1,6 @@
 const assert = require('power-assert');
 const Comment = require('../../models/Comment');
 
-assert;
-Comment;
 describe('Comment.createCommentメソッドの作成', () => {
   it('Comment.createCommentはメソッドである', () => {
     assert.equal(typeof Comment.createComment, 'function');

266eb83c15b24fcfd0a94a4750a733276a63b24d
atlansien <atlansien@yahoo.co.jp>
2019-04-15 14:33:27 +0900

    Comment.createCommentはメソッドであるテスト

--- test/models/createComment.test.js
+++ test/models/createComment.test.js
@@ -3,3 +3,8 @@ const Comment = require('../../models/Comment');
 
 assert;
 Comment;
+describe('Comment.createCommentメソッドの作成', () => {
+  it('Comment.createCommentはメソッドである', () => {
+    assert.equal(typeof Comment.createComment, 'function');
+  });
+});

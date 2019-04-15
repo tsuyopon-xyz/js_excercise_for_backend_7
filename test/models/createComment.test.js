4e91fc0b11f1f60b6abc05c33309259a529ee382
atlansien <atlansien@yahoo.co.jp>
2019-04-15 14:40:36 +0900

    usernameにプロパティ値が入ってない場合エラーが返されるテスト実装、現在RED

--- test/models/createComment.test.js
+++ test/models/createComment.test.js
@@ -7,4 +7,16 @@ describe('Comment.createCommentメソッドの作成', () => {
   it('Comment.createCommentはメソッドである', () => {
     assert.equal(typeof Comment.createComment, 'function');
   });
+  it('usernameにプロパティ値が入ってない場合エラーが返される', () => {
+    const dataList = [{}, { body: 'test body' }];
+
+    dataList.forEach(data => {
+      try {
+        Comment.createComment(data);
+        assert.fail();
+      } catch (error) {
+        assert.equal(error.message, 'usernameは必須です');
+      }
+    });
+  });
 });

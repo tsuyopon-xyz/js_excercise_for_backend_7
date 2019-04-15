79847f19796100fcd0caa4837f1ff51131faa12e
atlansien <atlansien@yahoo.co.jp>
2019-04-15 14:20:33 +0900

    getCommentメソッドの名前変更(sを追加)

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,7 @@
 const Comment = require('../models/Comment');
 
 module.exports = {
-  getComment: (req, res) => {
+  getComments: (req, res) => {
     const storedComments = Comment.findAll();
 
     res.status(200).json(storedComments);

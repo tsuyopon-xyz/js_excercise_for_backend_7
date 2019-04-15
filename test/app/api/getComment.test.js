bd663cf31b7b3e018f7bafef8c120e7b0a9082d8
atlansien <atlansien@yahoo.co.jp>
2019-04-15 10:58:39 +0900

    json形式で渡されているかのテストなので.setの削除

--- test/app/api/getComment.test.js
+++ test/app/api/getComment.test.js
@@ -7,7 +7,6 @@ describe('test 「GET /api/comments」', () => {
   it('GETリクエストで返されたデータは適切である', async () => {
     const response = await request(app)
       .get('/api/comments')
-      .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200);
 

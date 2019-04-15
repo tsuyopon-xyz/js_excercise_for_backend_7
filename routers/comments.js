0236914fb28e5c5530f84e6ed92e42c885c24d0c
atlansien <atlansien@yahoo.co.jp>
2019-04-15 13:57:16 +0900

    controllersフォルダの名前修正

--- routers/comments.js
+++ routers/comments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controller/comments');
+const controller = require('../controllers/comments');
 
 router.route('/').get(controller.getComment);
 

06eb23fe38961e37218dfa28346ec38783772987
atlansien <atlansien@yahoo.co.jp>
2019-04-15 14:21:30 +0900

    getCommentメソッドの名前変更(sを追加)

--- routers/comments.js
+++ routers/comments.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/comments');
 
-router.route('/').get(controller.getComment);
+router.route('/').get(controller.getComments);
 
 module.exports = router;

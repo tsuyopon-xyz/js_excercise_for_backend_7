5f05fd1e6502bd9e59efcfbbdffcaec9773275f7
atlansien <atlansien@yahoo.co.jp>
2019-04-16 17:19:09 +0900

    body-parserのインストール、動作するように設定

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = require('./routers/comments');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api/comments', router);
 

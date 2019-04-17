d9fb200f9004e897781f88416fcc00741994e0c0
atlansien <atlansien@yahoo.co.jp>
2019-04-17 23:38:21 +0900

    bodyを送らなかった場合400エラーを返す、テストOK

--- test/app/api/postComment.test.js
+++ test/app/api/postComment.test.js
@@ -15,4 +15,17 @@ describe('test 「POST /api/comments」', () => {
 
     assert.deepEqual(response.body, { message: 'usernameは必須です' });
   });
+  it('bodyを送らなかった場合400エラーが返る', async () => {
+    const data = { username: 'test user' };
+
+    const response = await requestHelper
+      .request({
+        method: 'post',
+        endPoint: '/api/comments',
+        statusCode: 400,
+      })
+      .send(data);
+
+    assert.deepEqual(response.body, { message: 'bodyは必須です' });
+  });
 });

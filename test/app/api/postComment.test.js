b643f7847851c19855a100a97a1ed22e35516c54
atlansien <atlansien@yahoo.co.jp>
2019-04-17 23:51:18 +0900

    適切なデータが送られた際の配列の確認テスト追加、結果OK(GREEN)

--- test/app/api/postComment.test.js
+++ test/app/api/postComment.test.js
@@ -1,6 +1,16 @@
 const requestHelper = require('../../../helper/requestHelper');
 const assert = require('power-assert');
 
+const getComments = async () => {
+  const response = await requestHelper.request({
+    method: 'get',
+    endPoint: '/api/comments',
+    statusCode: 200,
+  });
+
+  return response.body;
+};
+
 describe('test 「POST /api/comments」', () => {
   it('usernameを送らなかった場合400エラーが返る', async () => {
     const data = { body: 'test body' };
@@ -28,7 +38,9 @@ describe('test 「POST /api/comments」', () => {
 
     assert.deepEqual(response.body, { message: 'bodyは必須です' });
   });
-  it('適切なデータを送った場合、新規作成されたコメントが一件返ってくる', async () => {
+  it('適切なデータを送った場合、新規作成されたコメントが一件返ってくる、また配列内には新規作成されたコメントが一件格納されている', async () => {
+    const oldComments = await getComments();
+
     const data = { username: 'user', body: 'body' };
 
     const response = await requestHelper
@@ -47,5 +59,9 @@ describe('test 「POST /api/comments」', () => {
       createdAt: comment.createdAt,
       updatedAt: comment.updatedAt,
     });
+
+    const currentComments = await getComments();
+
+    assert.equal(oldComments.length + 1, currentComments.length);
   });
 });

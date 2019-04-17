c00805c202300809cfb83532175ca4294b2a0907
atlansien <atlansien@yahoo.co.jp>
2019-04-17 23:05:30 +0900

    supertest用のhelperファイル作成

---
+++ helper/requestHelper.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+request;
+const app = require('../app');
+app;
+module.exports = {
+  request: ({ method, endPoint, statusCode }) => {
+    method;
+    endPoint;
+    statusCode;
+  },
+};

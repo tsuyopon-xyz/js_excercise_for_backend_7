ef3cb6eba722b1f7cce43283b67ee723ced3d3aa
atlansien <atlansien@yahoo.co.jp>
2019-04-21 11:07:42 +0900

    putCommentをテストするための準備

--- test/app/api/putComment.test.js
+++ test/app/api/putComment.test.js
@@ -1,5 +1,28 @@
 const requestHelper = require('../../../helper/requestHelper');
 const assert = require('power-assert');
 
+const getComments = async () => {
+  const response = await requestHelper.request({
+    method: 'get',
+    endPoint: '/api/comments',
+    statusCode: 200,
+  });
+  return response;
+};
+
+const putComment = async (code, data) => {
+  const response = await requestHelper
+    .request({
+      method: 'put',
+      endPoint: '/api/comments/:id',
+      statusCode: code,
+    })
+    .send(data);
+  return response;
+};
+
+describe('TEST 「PUT api/comments/:id」', () => {});
 requestHelper;
 assert;
+getComments();
+putComment();

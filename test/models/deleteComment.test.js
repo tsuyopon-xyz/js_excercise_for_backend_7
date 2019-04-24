7b8b0f047bc28bb05d2a7761b4ce28a91112af36
atlansien <atlansien@yahoo.co.jp>
2019-04-24 22:26:43 +0900

    deleteCommentはメソッドであるテスト、GREEN

--- test/models/deleteComment.test.js
+++ test/models/deleteComment.test.js
@@ -1,5 +1,10 @@
 const assert = require('power-assert');
 const Comment = require('../../models/Comment');
 
+describe('Comment.deleteCommentのテスト', () => {
+  it('Comment.deleteCommentはメソッドである', () => {
+    assert.strictEqual(typeof Comment.deleteComment, 'function');
+  });
+});
 assert;
 Comment;

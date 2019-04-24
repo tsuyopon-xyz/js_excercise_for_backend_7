faa47baf8639299b3aecff324b003cb8217cbf6d
atlansien <atlansien@yahoo.co.jp>
2019-04-24 22:34:48 +0900

    idの引数に不正な値が入っていた場合のテスト、現在RED

--- test/models/deleteComment.test.js
+++ test/models/deleteComment.test.js
@@ -5,6 +5,28 @@ describe('Comment.deleteCommentのテスト', () => {
   it('Comment.deleteCommentはメソッドである', () => {
     assert.strictEqual(typeof Comment.deleteComment, 'function');
   });
+  it('idの引数に不正な値が入っていた場合、エラーが返る', () => {
+    const invalidIdList = [
+      { id: 0 },
+      { id: -1 },
+      { id: null },
+      { id: {} },
+      { id: [] },
+      { id: '1' },
+    ];
+
+    invalidIdList.forEach(id => {
+      try {
+        Comment.deleteComment(id);
+        assert.fail();
+      } catch (error) {
+        assert.strictEqual(
+          error.message,
+          'idに適切でない値が入っています、1以上の数字を入れてください'
+        );
+      }
+    });
+  });
 });
 assert;
 Comment;

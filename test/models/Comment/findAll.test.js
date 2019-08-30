79d10bd5321c223b15d556ca2a0da8449968bec3
cedrictaro-g <cedric.tarou@gmail.com>
2019-08-30 20:55:09 +0900

    complete implement the create method

--- test/models/Comment/findAll.test.js
+++ test/models/Comment/findAll.test.js
@@ -4,7 +4,5 @@ const Comment = require('../../../models/Comment');
 describe('Comment.findAllメソッドのテスト', () => {
   it('配列の決められたデータ構造でCommentが格納されている', () => {
     const comments = Comment.findAll();
-
-    console.log(comments, '@@@@@@@');
   });
 });
